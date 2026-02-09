da94642cc36f5268a3e08f3727d943a104aade7c
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2026-02-09 21:42:26 +1100

    Add check for REDUX BQR TSV

--- lib/SampleSheet.groovy
+++ lib/SampleSheet.groovy
@@ -259,19 +259,22 @@ class SampleSheet {
         def bam_path = meta_sample[Constants.FileType.BAM_REDUX]
         def bam_dir = bam_path.getParent().toUriString()
 
-        // Get user specified TSV paths
         def jitter_tsv = meta_sample[Constants.FileType.REDUX_JITTER_TSV]
         def ms_tsv = meta_sample[Constants.FileType.REDUX_MS_TSV]
+        def bqr_tsv = meta_sample[Constants.FileType.REDUX_BQR_TSV]
 
         // If TSV paths not provided, default to TSV paths in the same dir as the BAM
         def sample_id = meta_sample.getOrDefault('longitudinal_sample_id', meta_sample['sample_id'])
-        jitter_tsv = jitter_tsv ?: "${bam_dir}/${sample_id}.jitter_params.tsv"
-        ms_tsv = ms_tsv ?: "${bam_dir}/${sample_id}.ms_table.tsv.gz"
 
-        jitter_tsv = nextflow.Nextflow.file(jitter_tsv)
-        ms_tsv = nextflow.Nextflow.file(ms_tsv)
+        jitter_tsv = jitter_tsv ?: nextflow.Nextflow.file("${bam_dir}/${sample_id}.redux.jitter_params.tsv")
+        ms_tsv = ms_tsv ?: nextflow.Nextflow.file("${bam_dir}/${sample_id}.redux.ms_table.tsv.gz")
+        bqr_tsv = bqr_tsv ?: nextflow.Nextflow.file("${bam_dir}/${sample_id}.redux.bqr.tsv")
 
+        // Check for missing TSV files
         def missing_tsvs = [:]
+        if (!bqr_tsv.exists()) {
+            missing_tsvs[Constants.FileType.REDUX_BQR_TSV] = bqr_tsv
+        }
         if (!jitter_tsv.exists()) {
             missing_tsvs[Constants.FileType.REDUX_JITTER_TSV] = jitter_tsv
         }
@@ -283,21 +286,17 @@ class SampleSheet {
 
             def error_message = []
 
-            error_message.add("When only specifying filetype ${Constants.FileType.BAM_REDUX} in the sample sheet, make sure the REDUX BAM and TSVs are in the same dir:")
-            error_message.add("${bam_path.toUriString()} (${Constants.FileType.BAM_REDUX})")
-            missing_tsvs.each { error_message.add("${it.value} (missing expected ${it.key})") }
+            missing_tsvs.each { error_message.add("Missing ${it.key}: ${it.value}") }
             error_message.add("")
-            error_message.add(
-                "Alternatively, provide the TSV paths in the sample sheet using filetype values: " +
-                    "${Constants.FileType.REDUX_JITTER_TSV}, " +
-                    "${Constants.FileType.REDUX_MS_TSV}"
-            )
+            error_message.add("Make sure the REDUX BAM and TSVs are in the same dir, or explicitly provide")
+            error_message.add("the TSV paths in the sample sheet using filetype values: ${missing_tsvs.keySet().join(", ")}")
 
             log.error error_message.join("\n")
             Nextflow.exit(1)
         }
 
         // Set parsed REDUX TSV paths in metadata object
+        meta_sample[Constants.FileType.REDUX_BQR_TSV] = bqr_tsv
         meta_sample[Constants.FileType.REDUX_JITTER_TSV] = jitter_tsv
         meta_sample[Constants.FileType.REDUX_MS_TSV] = ms_tsv
     }
