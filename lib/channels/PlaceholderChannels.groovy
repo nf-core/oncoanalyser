c223e366131a73adf45c0b4274bbdacab8a40dd7
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2026-04-22 16:49:58 +1000

    Common: Add static types to PlaceholderChannel class members

--- lib/channels/PlaceholderChannels.groovy
+++ lib/channels/PlaceholderChannels.groovy
@@ -2,37 +2,37 @@ package channels
 
 class PlaceholderChannels {
 
-    private static createPlaceholderChannel(ch, n_items) {
+    private static Object createPlaceholderChannel(Object ch, int size) {
         // Given a channel containing metadata with the structure `[meta, ...]` or atomic: `meta`,
-        // Create a channel with the form e.g. `[meta, [], []]` if n_items=2
+        // Create a channel with the form e.g. `[meta, [], []]` if size=2
         return ch.map { d ->
             def meta = (d instanceof List) ? d[0] : d
 
             def output = [meta]
-            n_items.times { output.add([]) }
+            size.times { output.add([]) }
 
             return output
         }
     }
 
-    public static final N_ITEMS_TOOL_DIR = 1 // [ meta, dir ]
-    public static final N_ITEMS_BAM_BAI = 2 // [ meta, bam, bai ]
-    public static final N_ITEMS_VCF_TBI = 2 // [ meta, vcf, tbi ]
-    public static final N_ITEMS_REDUX_TSV_LIST = 1 // [ meta, [redux_tsv, ...] ]
+    public static final int N_ITEMS_TOOL_DIR = 1 // [ meta, dir ]
+    public static final int N_ITEMS_BAM_BAI = 2 // [ meta, bam, bai ]
+    public static final int N_ITEMS_VCF_TBI = 2 // [ meta, vcf, tbi ]
+    public static final int N_ITEMS_REDUX_TSV_LIST = 1 // [ meta, [redux_tsv, ...] ]
 
-    public static toolDir(ch) {
+    public static Object toolDir(Object ch) {
         return createPlaceholderChannel(ch, N_ITEMS_TOOL_DIR)
     }
 
-    public static bamBai(ch) {
+    public static Object bamBai(Object ch) {
         return createPlaceholderChannel(ch, N_ITEMS_BAM_BAI)
     }
 
-    public static vcfTbi(ch) {
+    public static Object vcfTbi(Object ch) {
         return createPlaceholderChannel(ch, N_ITEMS_VCF_TBI)
     }
 
-    public static reduxTsvs(ch) {
+    public static Object reduxTsvs(Object ch) {
         return createPlaceholderChannel(ch, N_ITEMS_REDUX_TSV_LIST)
     }
 }
